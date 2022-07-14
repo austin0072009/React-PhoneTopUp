39d35109bf6108cfaad98e7dcb4399da91c4d37f
AusintJagua <46783484+austin0072009@users.noreply.github.com>
2022-07-15 00:05:46 +0800

    feat(master): added author header to the code

---
+++ backend/utils/sign.js
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sign.js                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: austin0072009 <2001beijing@163.com, aus    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/15 00:00:23 by austin00720       #+#    #+#             */
+/*   Updated: 2022/07/15 00:00:23 by austin00720      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+import { appid, secret } from '../config';
+
+
+
+
+
+// 详情参考微信开放文档：https://developers.weixin.qq.com/doc/offiaccount/Basic_Information/Get_access_token.html
+// https请求方式: GET https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=APPID&secret=APPSECRET
+
