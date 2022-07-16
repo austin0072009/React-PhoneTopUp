af7b29e5d238727f5b41376b2e79603098c931eb
AusintJagua <46783484+austin0072009@users.noreply.github.com>
2022-07-16 17:45:33 +0800

    feat(master):added menu setting post req

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   index.js                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: austin0072009 <2001beijing@163.com>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/16 17:29:51 by austin00720       #+#    #+#             */
+/*   Updated: 2022/07/16 17:44:44 by austin00720      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 var express = require('express');
 var router = express.Router();
 var sha1 = require("sha1");
-var {sign} = require('../utils/sign');
-
+var {sign,getTicket} = require('../utils/sign');
+var axios = require("axios");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -38,6 +50,35 @@ router.get('/jsapi',async function(req,res){
   console.log('config',conf);
   res.send(conf);
 
+
+})
+
+router.post('/createMenu',async function(req,res){
+
+  var post_data =  {
+    "button":[
+    {	
+         "type":"view",
+         "name":"话费充值",
+         "url":"http://jiaguo.tcjy33.cn/"
+     },
+     {
+        "type":"click",
+        "name":"关于我们",
+        "key":"about"
+     }
+    ]
+};
+
+
+var {access_token} = await getTicket(); 
+
+
+var url = `https://api.weixin.qq.com/cgi-bin/menu/create?access_token=${access_token}`;
+axios.post(url,post_data)
+.then(res=>{
+    console.log('res=>',res);            
+})
 
 })
 
