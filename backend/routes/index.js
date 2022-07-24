b45d28e425e26f57b55621e84eb6e9cb89001fad
AusintJagua <46783484+austin0072009@users.noreply.github.com>
2022-07-24 18:00:44 +0800

    feat: wechat jsapi v3 paysign get code added

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,44 +6,46 @@
 /*   By: austin0072009 <2001beijing@163.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/16 17:29:51 by austin00720       #+#    #+#             */
-/*   Updated: 2022/07/22 13:18:44 by austin00720      ###   ########.fr       */
+/*   Updated: 2022/07/24 18:00:24 by austin00720      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 var express = require('express');
 var router = express.Router();
 var sha1 = require("sha1");
-var {sign,getTicket} = require('../utils/sign');
+var { sign, getTicket } = require('../utils/sign');
 var axios = require("axios");
 var cors = require("cors");
 var {
   appid,
   secret
 } = require('../config/index');
+var crypto = require('crypto');
+var fs = require('fs');
 
 
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
 
   console.log("Wechat Check");
   console.log(req.query);
-  var {signature,timestamp,nonce,echostr} = req.query;
+  var { signature, timestamp, nonce, echostr } = req.query;
 
   var token = "Austin";
-  var arrSort = [token,timestamp,nonce];
+  var arrSort = [token, timestamp, nonce];
   arrSort.sort();
 
   var str = arrSort.join("");
   var shaStr = sha1(str);
 
-  
+
 
   console.log(shaStr);
 
-  if(signature === shaStr){
-    res.set('Content-Type','text/plain');
+  if (signature === shaStr) {
+    res.set('Content-Type', 'text/plain');
     res.send(echostr);
   }
   else res.send("Nothing Happen, But this it the right route");
@@ -53,63 +55,77 @@ router.get('/', function(req, res, next) {
 router.use(cors());
 
 
-router.get('/jsapi',async function(req,res){
+router.get('/jsapi', async function (req, res) {
 
   let url = decodeURIComponent(req.query.url);
   let conf = await sign(url);
-  
-  console.log('config',conf);
+
+  console.log('config', conf);
   res.send(conf);
 
 
 })
 
 //服务器创建自定义菜单
-router.get('/createMenu',async function(req,res){
+router.get('/createMenu', async function (req, res) {
 
-  var post_data =  {
-    "button":[
-    {	
-         "type":"view",
-         "name":"话费充值",
-         "url":"http://jiaguo.tcjy33.cn/"
-     },
-     {
-        "type":"click",
-        "name":"关于我们",
-        "key":"about"
-     }
+  var post_data = {
+    "button": [
+      {
+        "type": "view",
+        "name": "话费充值",
+        "url": "http://jiaguo.tcjy33.cn/"
+      },
+      {
+        "type": "click",
+        "name": "关于我们",
+        "key": "about"
+      }
     ]
-};
+  };
 
 
-var {access_token} = await getTicket(); 
+  var { access_token } = await getTicket();
 
 
-var url = `https://api.weixin.qq.com/cgi-bin/menu/create?access_token=${access_token}`;
-axios.post(url,post_data)
-.then(res=>{
-    console.log('res=>',res);            
-})
+  var url = `https://api.weixin.qq.com/cgi-bin/menu/create?access_token=${access_token}`;
+  axios.post(url, post_data)
+    .then(res => {
+      console.log('res=>', res);
+    })
 
 })
 
 
 //服务器用code换取accesstoken
-router.post('/exchangeCode',async function(req,res){
+router.post('/exchangeCode', async function (req, res) {
 
-  console.log(req.query);
   console.log(req.body);
-  var code = req.query.code;
-  var state = req.query.state;
-  var appid = req.query.appid;
-  axios.get(`https://api.weixin.qq.com/sns/oauth2/access_token?appid=${appid}&secret=${secret}&code=${code}&grant_type=authorization_code`)
-  .then(data=>{
-    console.log(data);
-    res.send(data);
-  })
+  var code = req.body.code;
 
-  res.status(200).send(success);
+
+  console.log("code", code);
+  axios.get(`https://api.weixin.qq.com/sns/oauth2/access_token?appid=${appid}&secret=${secret}&code=${code}&grant_type=authorization_code`)
+    .then(data => {
+      console.log(data);
+
+      res.status(200).send(data);
+    })
+
+})
+
+
+router.post('/getPaySign', async function (req, res) {
+
+  var { nonceStr, timestamp } = req.body;
+
+  const message = `GET\n/v3/certificates\n${timestamp}\n${nonceStr}\n\n`;
+  const signature = crypto.createSign('RSA-SHA256').update(message, 'utf-8').sign(fs.readFileSync('./pem/apiclient_key.pem').toString(), 'base64');
+
+  console.log(signature);
+
+  res.status(204).send(signature);
+
 })
 
 module.exports = router;
--- topupapp/src/pages/Home.js
+++ topupapp/src/pages/Home.js
@@ -32,11 +32,13 @@ import wx from 'weixin-js-sdk';
 import axios from 'axios';
 import { appid, secret } from "../config/index"
 
+
 export default function Home() {
 
 
     const [phone, setPhone] = useState();
     const [amount, setAmount] = useState();
+    const [openid, setOpenId] = useState();
 
     useEffect(() => {
         console.log("author", window.austin);
@@ -100,6 +102,8 @@ export default function Home() {
         var time = new Date().getTime();
         console.log(phone, amount[0], time);
         console.log("submit test");
+        //console.log(createNonceStr(),createTimeStamp());
+
 
 
         //由于orc跨域问题 这一步必须要在后端处理，这里要post请求到后端
@@ -112,45 +116,82 @@ export default function Home() {
         //     console.log(openid);
         // })
 
-        var backendUrl = "localhost/exchangeCode"
-        await axios.post(backendUrl, {
+        //总共要对后端进行三次请求分别 1.用code换取用户的openid
+        //2.jsapi调起支付 得到一个prepay_id
+        //3.将此前所有参数进行签名 得到paysign
+
+        //Step1 code换取openid
+
+        var backendUrl = "http://web.tcjy33.cn/exchangeCode"
+        console.log("code", window.code);
+
+
+        var { openid } = await axios.post(backendUrl, {
             appid: appid,
-            secret : secret,
-            code : window.code
+            secret: secret,
+            code: window.code
         }).then(function (response) {
             console.log(response);
-          })
-          .catch(function (error) {
-            console.log(error);
-          });
+        })
+            .catch(function (error) {
+                console.log(error);
+            });
 
 
-
-
+        //Step 2 调起支付 获得prepay_id
         var payment_data =
         {
             "mchid": "1628040916",
             "out_trade_no": getOrderNumber(),
             "appid": appid,
             "description": "亚洲未来科技-话费充值-缅甸话费充值",
-            "notify_url": "https://weixin.qq.com/",
+            "notify_url": "http://web.tcjy33.cn/notify",
             "amount": {
                 "total": amount,
                 "currency": "CNY"
             },
             "payer": {
-                "openid": "o4GgauInH_RCEdvrrNGrntXDuXXX"
+                "openid": openid
             }
         }
 
+        var { prepay_id } = await axios.post("https://api.mch.weixin.qq.com/v3/pay/transactions/jsapi", payment_data).then(function (response) {
+            console.log(response);
+        })
+            .catch(function (error) {
+                console.log(error);
+            });
+
+        //Step3 生成支付签名，这一步需要在微信支付商户平台，得到商户v3支付的私钥，并用私钥进行签名
+        //也是在后端中处理，发请求到后端
+        var payUrl = "http://web.tcjy33.cn/getPaySign";
+        const nonceStr = Math.random().toString(36).slice(-10);
+        const timestamp = (new Date().getTime() / 1000).toFixed(0);
+
+        //以下操作需要在后端完成
+        //const message = `GET\n/v3/certificates\n${timestamp}\n${nonceStr}\n\n`;
+        // const signature = crypto.createSign('RSA-SHA256').update(message, 'utf-8').sign(fs.readFileSync('../pem/apiclient_key').toString(), 'base64');
+
+        var signature = await axios.post(payUrl, {
+            nonceStr,
+            timestamp
+
+        }).then(function (response) {
+            console.log(response);
+        })
+            .catch(function (error) {
+                console.log(error);
+            });
+
         wx.chooseWXPay({
-            timestamp: 0, // 支付签名时间戳，注意微信 jssdk 中的所有使用 timestamp 字段均为小写。但最新版的支付后台生成签名使用的 timeStamp 字段名需大写其中的 S 字符
-            nonceStr: '', // 支付签名随机串，不长于 32 位
-            package: '', // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=\*\*\*）
-            signType: '', // 微信支付V3的传入 RSA ,微信支付V2的传入格式与V2统一下单的签名格式保持一致
-            paySign: '', // 支付签名
+            timestamp: timestamp, // 支付签名时间戳，注意微信 jssdk 中的所有使用 timestamp 字段均为小写。但最新版的支付后台生成签名使用的 timeStamp 字段名需大写其中的 S 字符
+            nonceStr: nonceStr, // 支付签名随机串，不长于 32 位
+            package: "prepay_id=" + prepay_id, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=\*\*\*）
+            signType: "RSA", // 微信支付V3的传入 RSA ,微信支付V2的传入格式与V2统一下单的签名格式保持一致
+            paySign: signature, // 支付签名
             success: function (res) {
                 // 支付成功后的回调函数
+                console.log(res);
             }
         });
 
