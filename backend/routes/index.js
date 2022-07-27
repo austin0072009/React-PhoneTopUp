c3dbe3aec2c852491e7f1ef20dcf660ff2d8b119
AusintJagua <46783484+austin0072009@users.noreply.github.com>
2022-07-27 16:48:39 +0800

    fix(bug): 缺少双引号message

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,7 +6,7 @@
 /*   By: austin0072009 <2001beijing@163.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/16 17:29:51 by austin00720       #+#    #+#             */
-/*   Updated: 2022/07/27 16:11:33 by austin00720      ###   ########.fr       */
+/*   Updated: 2022/07/27 16:48:25 by austin00720      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -142,7 +142,7 @@ router.post('/getPaySign', async function (req, res) {
   var { appid, amount, openid, nonceStr, timestamp } = req.body;
 
   // const message = `GET\n/v3/pay/transactions/jsapi\n${timestamp}\n${nonceStr}\n\n`;
-  const message = `POST\n/v3/pay/transactions/jsapi\n${timestamp}\n${nonceStr}\n{"mchid":"1628040916","out_trade_no":${orderNumber},"appid":${appid},"description":"亚洲未来科技-话费充值-缅甸话费充值","notify_url":"http://web.tcjy33.cn/notify","amount":{"total":${amount},"currency":"CNY"},"payer":{"openid":${openid}}}\n`;
+  const message = `POST\n/v3/pay/transactions/jsapi\n${timestamp}\n${nonceStr}\n{"mchid":"1628040916","out_trade_no":"${orderNumber}","appid":"${appid}","description":"亚洲未来科技-话费充值-缅甸话费充值","notify_url":"http://web.tcjy33.cn/notify","amount":{"total":"${amount}","currency":"CNY"},"payer":{"openid":"${openid}"}}\n`;
 
   const signature = crypto.createSign('RSA-SHA256').update(message, 'utf-8').sign(fs.readFileSync('./pem/apiclient_key.pem').toString(), 'base64');
 
@@ -152,6 +152,8 @@ router.post('/getPaySign', async function (req, res) {
 
 })
 
+
+//下单接口
 router.post('/getPrepayId', async function (req, res) {
 
   var { appid, amount, openid, nonceStr, timestamp } = req.body;
@@ -173,7 +175,7 @@ router.post('/getPrepayId', async function (req, res) {
       "openid": openid
     }
   }
-  const message = `POST\n/v3/pay/transactions/jsapi\n${timestamp}\n${nonceStr}\n{"mchid":"1628040916","out_trade_no":${orderNumber},"appid":${appid},"description":"亚洲未来科技-话费充值-缅甸话费充值","notify_url":"http://web.tcjy33.cn/notify","amount":{"total":${amount},"currency":"CNY"},"payer":{"openid":${openid}}}\n`;
+  const message = `POST\n/v3/pay/transactions/jsapi\n${timestamp}\n${nonceStr}\n{"mchid":"1628040916","out_trade_no":"${orderNumber}","appid":"${appid}","description":"亚洲未来科技-话费充值-缅甸话费充值","notify_url":"http://web.tcjy33.cn/notify","amount":{"total":"${amount}","currency":"CNY"},"payer":{"openid":"${openid}"}}\n`;
   console.log("message:", message);
   const signature = crypto.createSign('RSA-SHA256').update(message, 'utf-8').sign(fs.readFileSync('./pem/apiclient_key.pem').toString(), 'base64');
   const serial_no = process.env.SERIAL_NO;
