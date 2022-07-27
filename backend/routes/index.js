a3110d5f451cc3f9bb32ac4dd8598649c05be7a6
AusintJagua <46783484+austin0072009@users.noreply.github.com>
2022-07-27 14:34:43 +0800

    fix(backend)bug test

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,7 +6,7 @@
 /*   By: austin0072009 <2001beijing@163.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/16 17:29:51 by austin00720       #+#    #+#             */
-/*   Updated: 2022/07/27 14:23:58 by austin00720      ###   ########.fr       */
+/*   Updated: 2022/07/27 14:34:21 by austin00720      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -152,7 +152,7 @@ router.post('/getPrepayId', async function (req, res) {
 
   var { appid, amount, openid, nonceStr, timestamp } = req.body;
 
-  var orderNumber = getOrderNumber();
+  var orderNumber = getOrderNumber().toString();
   console.log(req.body);
   var payment_data =
   {
@@ -169,20 +169,7 @@ router.post('/getPrepayId', async function (req, res) {
       "openid": openid
     }
   }
-  const message = `POST\n/v3/certificates\n${timestamp}\n${nonceStr}\n{
-    "mchid": "1628040916",
-    "out_trade_no": ${orderNumber},
-    "appid": ${appid},
-    "description": "亚洲未来科技-话费充值-缅甸话费充值",
-    "notify_url": "http://web.tcjy33.cn/notify",
-    "amount": {
-      "total": ${amount},
-      "currency": "CNY"
-    },
-    "payer": {
-      "openid": ${openid}
-    }
-  }\n`;
+  const message = `POST\n/v3/certificates\n${timestamp}\n${nonceStr}\n{"mchid":"1628040916","out_trade_no":${orderNumber},"appid":${appid},"description":"亚洲未来科技-话费充值-缅甸话费充值","notify_url":"http://web.tcjy33.cn/notify","amount":{"total":${amount},"currency":"CNY"},"payer":{"openid":${openid}}}\n`;
   console.log("message:", message);
   const signature = crypto.createSign('RSA-SHA256').update(message, 'utf-8').sign(fs.readFileSync('./pem/apiclient_key.pem').toString(), 'base64');
   const serial_no = process.env.SERIAL_NO;
