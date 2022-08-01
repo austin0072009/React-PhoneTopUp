4fe539f5179ce1ba24a24177acc53a355db65cf8
AusintJagua <46783484+austin0072009@users.noreply.github.com>
2022-08-01 13:45:23 +0800

    feat(topupapp): added Record page frontend

--- topupapp/src/pages/Record.js
+++ topupapp/src/pages/Record.js
@@ -1,26 +1,29 @@
 import "./Record.css"
 import React, { useState } from 'react'
 import { InfiniteScroll, List } from 'antd-mobile'
-
+// import { mockRequest } from "../components/mockRequest";
 
 export default function Record() {
 
-    // const [data, setData] = useState < string[] > ([])
-    // const [hasMore, setHasMore] = useState(true)
+    const [data, setData] = useState([]);
+    const [hasMore, setHasMore] = useState(true);
     // async function loadMore() {
     //     const append = await mockRequest()
     //     setData(val => [...val, ...append])
     //     setHasMore(append.length > 0)
     // }
+    async function loadOrder(){
+        
+    }
 
-    return (<div></div>);
-        // <>
-        //     <List>
-        //         {data.map((item, index) => (
-        //             <List.Item key={index}>{item}</List.Item>
-        //         ))}
-        //     </List>
-        //     <InfiniteScroll loadMore={loadMore} hasMore={hasMore} />
-        // </>
+    return (
+        <div className="record">
+            <List header="充值记录">
+
+
+            </List>
+            <InfiniteScroll  hasMore={hasMore} />
+        </div>
+    );
     
 }
