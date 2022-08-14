1d9de43b6b8e22569ea80485b2f1c64d7b47b748
AusintJagua <46783484+austin0072009@users.noreply.github.com>
2022-08-14 19:56:10 +0800

    feat: Personal page text added

--- topupapp/src/pages/PersonalCenter.js
+++ topupapp/src/pages/PersonalCenter.js
@@ -1,6 +1,7 @@
 import "./personalInfo.css"
 import React, { useState,useEffect } from 'react'
-import { List, Avatar, Space } from 'antd-mobile'
+import { List, Avatar, Space,Card } from 'antd-mobile'
+import { ListItem } from "antd-mobile/es/components/list/list-item";
 
 export default function PersonalCenter() {
 
@@ -29,6 +30,16 @@ export default function PersonalCenter() {
                     </div>
 
                     </List.Item>
+                    <ListItem>
+
+                    <Card>
+                        由于个人精力有限，许多扩展功能尚未开展。<br/>
+                        本人希望打造依赖于微信支付的世界各国华人话费充值的便民系统 <br/>
+                        本人作为缅甸人的资源有限，只能拥有缅甸及其周边国家的话费充值渠道 <br/>
+                        不论是开发还是提供资源也好，欢迎您联系我，我们一起追求梦想 ！
+                    </Card>
+
+                    </ListItem>
                 </List>
 
             </div>
