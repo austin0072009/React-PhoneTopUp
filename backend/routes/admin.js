70140604251b633fd9172af92192ceb181bf4610
AusintJagua <46783484+austin0072009@users.noreply.github.com>
2022-08-16 02:24:46 +0800

    backend(adminjs): adminsystem updated

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -4,15 +4,38 @@ const AdminJSMongoose = require('@adminjs/mongoose')
 
 
 var mongooseDb = require("../lib/mongoose");
-var tickets = require('../lib/schema_tickets');
+var tickets = require('../lib/ticketModel');
+var orders = require("../lib/orderModel");
 
 AdminJS.registerAdapter(AdminJSMongoose)
 
-const adminJs = new AdminJS({
+// const adminJs = new AdminJS({
+//   databases: [mongooseDb],
+//   rootPath: '/admin',
+// })
+const contentParent = {
+  name: '业务操作',
+  icon: 'Accessibility',
+}
+
+const adminJsOptions = {
   databases: [mongooseDb],
   rootPath: '/admin',
-})
+  resources: [
+    // { resource: tickets, options: { parent: contentParent } },
+    { resource: orders, options: { parent: contentParent ,listProperties: ['topup_Order_No','topup_Date','topup_Type','topup_Amount_Kyat',
+  'topup_Amount_Rmb','topup_Phone','topup_Order_State'] } },
+  ],
+  locale: {
+    translations: {
+      labels: {
+        orders: '订单'
+      }
+    }
+  },
+}
 
+const adminJs = new AdminJS(adminJsOptions);
 
 
 
