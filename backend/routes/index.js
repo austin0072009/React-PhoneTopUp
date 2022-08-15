2ccc464e019cb66be8c6fe17929c83634897e8cb
AusintJagua <46783484+austin0072009@users.noreply.github.com>
2022-08-15 14:11:00 +0800

    feat(backend): sign Error bug

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,7 +6,7 @@
 /*   By: austin0072009 <2001beijing@163.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/16 17:29:51 by austin00720       #+#    #+#             */
-/*   Updated: 2022/08/15 14:01:29 by austin00720      ###   ########.fr       */
+/*   Updated: 2022/08/15 14:10:45 by austin00720      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -254,17 +254,8 @@ router.post('/getPrepayId', async function (req, res) {
       "openid": openid
     }
   }
-  const message = `POST\n
-    /v3/pay/transactions/jsapi\n
-    ${timestamp}\n
-    ${nonceStr}\n
-    {"mchid":"1628040916","out_trade_no":"${orderNumber}",
-    "appid":"${appid}",
-    "description":"亚洲未来科技-话费充值-缅甸话费充值",
-    "notify_url":"http://web.tcjy33.cn/notify",
-    "amount":{"total":${total},
-    "currency":"CNY"},
-    "payer":{"openid":"${openid}"}}\n`;
+  const message = `POST\n/v3/pay/transactions/jsapi\n${timestamp}\n${nonceStr}\n{"mchid":"1628040916","out_trade_no":"${orderNumber}","appid":"${appid}","description":"亚洲未来科技-话费充值-缅甸话费充值","notify_url":"http://web.tcjy33.cn/notify","amount":{"total":${total},"currency":"CNY"},"payer":{"openid":"${openid}"}}\n`;
+
 
 
   console.log("message:", message);
