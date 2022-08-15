6e296a19dde428b20da2cac74e3a6885914f49d7
AusintJagua <46783484+austin0072009@users.noreply.github.com>
2022-08-15 19:40:35 +0800

    feat(topupapp): record bug

--- topupapp/src/pages/Record.js
+++ topupapp/src/pages/Record.js
@@ -60,7 +60,7 @@ export default function Record() {
 
                     {data.map((item, index) => (
                         <List.Item key={index}>下单时间：{moment((parseInt(item.topup_Date))).format("YYYY-MM-DD HH:mm:ss")
-                        } <br />充值量： {item.topup_Amount_Kyats}   类型： {item.topup_Type}<br /> 充值手机：{item.topup_Phone} <br /> 状态：{item.topup_Order_State} </List.Item>
+                        } <br />充值量： {item.topup_Amount_Kyat}   类型： {item.topup_Type}<br /> 充值手机：{item.topup_Phone} <br /> 状态：{item.topup_Order_State} </List.Item>
                     ))}
 
 
